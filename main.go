e42563a2e81c6c0db6ee941d12dac0d5eefe0136
Andy Provan <AndyProvan@gmail.com>
2019-10-18 22:21:32 +0100

    Complete Chapter 2

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ func main() {
 	fmt.Printf("P3\n%v %v \n255\n", nx, ny)
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			r := float64(i) / float64(nx)
-			g := float64(j) / float64(ny)
-			b := 0.2
-			ir := int(255.99 * r)
-			ig := int(255.99 * g)
-			ib := int(255.99 * b)
+			col := Vector{
+				float64(i) / float64(nx),
+				float64(j) / float64(ny),
+				0.2,
+			}
+			ir := int(255.99 * col.x)
+			ig := int(255.99 * col.y)
+			ib := int(255.99 * col.z)
 			fmt.Printf("%v %v %v\n", ir, ig, ib)
 		}
 	}
