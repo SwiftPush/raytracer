594f0e8d5d0cf71f1f49a93c984d194be12f642c
Andy Provan <AndyProvan@gmail.com>
2019-10-18 22:37:49 +0100

    Complete Chapter 3

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
+func color(ray Ray) Vector {
+	unitDirection := ray.direction.Normalise()
+	t := 0.5 * (unitDirection.y + 1.0)
+	return Vector{1, 1, 1}.MultiplyScalar(1.0 - t).Add(Vector{0.5, 0.7, 1.0}.MultiplyScalar(t))
+}
+
 func main() {
 	nx, ny := 200, 100
 	fmt.Printf("P3\n%v %v \n255\n", nx, ny)
+	lowerLeftCorner := Vector{-2, -1, -1}
+	horizontal := Vector{4, 0, 0}
+	vertical := Vector{0, 2, 0}
+	origin := Vector{0, 0, 0}
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			col := Vector{
-				float64(i) / float64(nx),
-				float64(j) / float64(ny),
-				0.2,
+			u := float64(i) / float64(nx)
+			v := float64(j) / float64(ny)
+			r := Ray{
+				origin,
+				lowerLeftCorner.Add(horizontal.MultiplyScalar(u).Add(vertical.MultiplyScalar(v))),
 			}
+			col := color(r)
 			ir := int(255.99 * col.x)
 			ig := int(255.99 * col.y)
 			ib := int(255.99 * col.z)
