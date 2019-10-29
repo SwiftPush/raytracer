4b38d971e24aa10adb1e8dffff87b2badfff2f75
Andy Provan <AndyProvan@gmail.com>
2019-10-29 22:29:48 +0000

    Remove unused function

--- diffuse.go
+++ diffuse.go
@@ -4,12 +4,6 @@ type Diffuse struct {
 	albedo Vector
 }
 
-func NewDiffuse(albedo Vector) Diffuse {
-	return Diffuse{
-		albedo,
-	}
-}
-
 func (d Diffuse) scatter(ray Ray, hitRecord HitRecord) (result bool, attenuation Vector, scattered Ray) {
 	target := hitRecord.p.Add(hitRecord.normal).Add(randomInUnitSphere())
 
