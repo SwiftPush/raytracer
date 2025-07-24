ce6c39e7d99df23c915f1088aefe27c57a922e5e
Andy Provan <git@provan.uk>
2025-07-24 22:51:11 +0100

    update the example scene

--- internal/scene.go
+++ internal/scene.go
@@ -10,14 +10,14 @@ type Scene struct {
 func exampleScene1(nx, ny int) Scene {
 	objects := HitableList{
 		[]Hitable{
-			Sphere{geometry.Vector{X: 0, Y: 0, Z: -1}, 0.5, Diffuse{geometry.Vector{X: 0.8, Y: 0.3, Z: 0.3}}},
-			Sphere{geometry.Vector{X: 0, Y: -100.5, Z: -1}, 100, Diffuse{geometry.Vector{X: 0.8, Y: 0.8, Z: 0}}},
-			Sphere{geometry.Vector{X: 1, Y: 0, Z: -1}, 0.5, Metal{geometry.Vector{X: 0.8, Y: 0.6, Z: 0.2}, 0.3}},
-			Sphere{geometry.Vector{X: -1, Y: 0, Z: -1}, 0.5, Dielectric{1.5}},
-			Sphere{geometry.Vector{X: -1, Y: 0, Z: -1}, -0.45, Dielectric{1.5}},
+			Sphere{geometry.Vector{X: 0, Y: 0, Z: -1}, 0.5, Diffuse{geometry.Vector{X: 0.8, Y: 0.15, Z: 0.15}}},    // middle sphere
+			Sphere{geometry.Vector{X: 0, Y: -100.5, Z: -1}, 100, Diffuse{geometry.Vector{X: 0.8, Y: 0.8, Z: 0.8}}}, // floor
+			Sphere{geometry.Vector{X: 1.1, Y: 0, Z: -1}, 0.5, Metal{geometry.Vector{X: 0.8, Y: 0.6, Z: 0.2}, 0.3}}, // right sphere
+			Sphere{geometry.Vector{X: -1, Y: 0, Z: -1}, 0.5, Dielectric{1.5}},                                      // glass sphere (1/2)
+			Sphere{geometry.Vector{X: -1, Y: 0, Z: -1}, -0.45, Dielectric{1.5}},                                    // glass sphere (2/2)
 		},
 	}
-	lookFrom := geometry.Vector{X: -2, Y: 2, Z: 1}
+	lookFrom := geometry.Vector{X: -3, Y: 1, Z: 4}
 	lookAt := geometry.Vector{X: 0, Y: 0, Z: -1}
 	camera := NewCamera(
 		lookFrom,
